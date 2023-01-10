13387a5abb32def1ad1d047adb6427ca19c1aeec
Emmanuel Eni <eejeba@gmail.com>
2023-01-10 12:15:15 +0100

    Essentially what was submitted for second semester exam, with minor modifications.

---
+++ database/connection.js
@@ -0,0 +1,19 @@
+const { connect, connection } = require('mongoose');
+const { config } = require('dotenv');
+
+// DOTENV CONFIG
+config();
+
+const DB_URI = process.env.DB_URI;
+
+mongoDB().catch(err => console.log(err.message));
+
+async function mongoDB() {
+  await connect(DB_URI);
+}
+
+connection.on('connected', () => {
+  console.log('MongoDB is connected, successfully');
+});
+
+module.exports = { mongoDB };
